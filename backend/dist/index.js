9614a2b6c99ba8c1c3d73055cafbe92d665e71ce
Abhinav <abhichaurasia016@gmail.com>
2026-04-16 00:56:29 +0530

    deployment changes

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,12 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+const env_1 = require("./lib/env");
+dotenv_1.default.config({
+    path: path_1.default.resolve(process.cwd(), "backend/.env")
+});
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 5000;
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Server running 🚀");
 });
+const PORT = Number(process.env.PORT);
+const rootDir = process.cwd();
+if (env_1.ENV.NODE_ENV === "production") {
+    const frontendPath = path_1.default.join(rootDir, "frontend", "dist");
+    app.use(express_1.default.static(frontendPath));
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path_1.default.join(frontendPath, "index.html"));
+    });
+}
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
