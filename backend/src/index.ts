4d751a46983a17282a62b52f251127ed67ac8a57
Abhinav <abhichaurasia016@gmail.com>
2026-04-16 00:16:08 +0530

    deployment config added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,34 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { ENV } from "./lib/env";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), "backend/.env")
+});
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Server running 🚀");
+app.get("/", (req, res) => {
+  res.send("Server running 🚀");
 });
 
+const PORT = Number(process.env.PORT);
+
+const rootDir = process.cwd();
+
+if (ENV.NODE_ENV === "production") {
+  const frontendPath = path.join(rootDir, "frontend", "dist");
+
+  app.use(express.static(frontendPath));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
