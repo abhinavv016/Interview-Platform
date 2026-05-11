522e66ade470c2e2d6a1ac2f18c995f0b4c63ba9
Abhinav <abhichaurasia016@gmail.com>
2026-05-11 16:28:40 +0530

    Remove backend dist folder from git

--- backend/dist/lib/clerk-webhook.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from 'express';
-export declare function handleClerkWebhook(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-//# sourceMappingURL=clerk-webhook.d.ts.map
--- backend/dist/lib/prisma.d.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from "../../node_modules/.prisma/client";
-declare const prisma: PrismaClient<import(".prisma/client/client", { with: { "resolution-mode": "import" } }).Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/client").DefaultArgs>;
-export default prisma;
-//# sourceMappingURL=prisma.d.ts.map
